e936f65f4636674672f6794d1c11259af80cc7b9
Milton Bravo <miltonbravo96@gmail.com>
2021-06-17 16:15:45 -0500

    controlador publico

---
+++ API JWT NETCORE/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_JWT_NETCORE.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    [AllowAnonymous]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost]
+        [Route("ping")]
+        public async Task<IActionResult> Ping()
+        {
+            return Ok("API Up");
+        }
+
+        [HttpGet]
+        [Route("version")]
+        public async Task<IActionResult> Version()
+        {
+            return Ok("Version 1");
+        }
+    }
+}
