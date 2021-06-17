6b20ef276c40c6eb9b0e9e3f00a5f2eed40ec181
Milton Bravo <miltonbravo96@gmail.com>
2021-06-17 17:25:51 -0500

    conexión a base de datos

--- API JWT NETCORE/Controllers/HomeController.cs
+++ API JWT NETCORE/Controllers/HomeController.cs
@@ -1,18 +1,26 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API_JWT_NETCORE.Controllers
 {
-    [Route("api")]
     [ApiController]
+    [Route("api")]    
     [AllowAnonymous]
     public class HomeController : ControllerBase
     {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpPost]
         [Route("ping")]
         public async Task<IActionResult> Ping()
@@ -25,6 +33,21 @@ namespace API_JWT_NETCORE.Controllers
         public async Task<IActionResult> Version()
         {
             return Ok("Version 1");
+        }
+
+        [HttpGet]
+        [Route("conexion")]
+        public async Task<IActionResult> Conexion()
+        {
+            string ok = null;
+            using(SqlConnection con = Models.Conexion.Conectar())
+            {
+                ok = "Conexión exitosa";
+            }
+
+            if (ok == null)
+                return BadRequest();
+            return Ok("Conexión exitosa");
         }
     }
 }
---
+++ API JWT NETCORE/Models/Conexion.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_JWT_NETCORE.Models
+{
+    public class Conexion
+    {
+        public static SqlConnection Conectar()
+        {
+            try
+            {
+                SqlConnection con = null;
+                string query = GetConnectionString();
+                con = new SqlConnection(query);
+                return con;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        private static string GetConnectionString()
+        {
+            try
+            {
+                string con = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ConnectionStrings")["DB"];
+                return con;
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+    }
+}
