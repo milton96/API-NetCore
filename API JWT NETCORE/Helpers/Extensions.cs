d08af3ce22245389244888c43753ced303db046c
Milton Bravo <miltonbravo96@gmail.com>
2021-06-17 20:38:47 -0500

    helpers agregados

---
+++ API JWT NETCORE/Helpers/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_JWT_NETCORE.Helpers
+{
+    public static class Extensions
+    {
+        public static T GetValor<T>(this SqlDataReader reader, int index)
+        {
+            return reader.IsDBNull(index) ? default : (T)reader.GetValue(index);
+        }
+    }
+}
---
+++ API JWT NETCORE/Helpers/RegexHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_JWT_NETCORE.Helpers
+{
+    public class RegexHelper
+    {
+        public const string Correo = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+    }
+}
