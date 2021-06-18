b0649be8f537be39b5d5aff7e25f0e6e3c83cbba
Milton Bravo <miltonbravo96@gmail.com>
2021-06-17 20:46:51 -0500

    helper para obtener configuraciones

---
+++ API JWT NETCORE/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_JWT_NETCORE.Helpers
+{
+    public class ConfigurationHelper
+    {
+        public static IConfigurationSection GetSection(string section)
+        {
+            return new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection(section);
+        }
+    }
+}
--- API JWT NETCORE/Models/Conexion.cs
+++ API JWT NETCORE/Models/Conexion.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using API_JWT_NETCORE.Helpers;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -28,7 +29,8 @@ namespace API_JWT_NETCORE.Models
         {
             try
             {
-                string con = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ConnectionStrings")["DB"];
+                //string con = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ConnectionStrings")["DB"];
+                string con = ConfigurationHelper.GetSection("ConnectionStrings")["DB"];
                 return con;
             }
             catch (Exception ex)
