d00f9718ccece561f767352fcf182b50d4e73710
Milton Bravo <miltonbravo96@gmail.com>
2021-06-18 14:44:33 -0500

    crear, obtener uno y obtener todos

--- API JWT NETCORE/Controllers/EmpleadoController.cs
+++ API JWT NETCORE/Controllers/EmpleadoController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using API_JWT_NETCORE.Helpers;
+using API_JWT_NETCORE.Models;
+using API_JWT_NETCORE.Requests;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -23,7 +26,92 @@ namespace API_JWT_NETCORE.Controllers
             }
             catch (Exception ex)
             {
-                return BadRequest(ex.Message);
+                return BadRequest(ex.ToLista());
+            }
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> Crear(UsuarioRequest usuario)
+        {
+            try
+            {
+                Rol rol = await Rol.ObtenerPorCodigo(usuario.Rol);
+                if (rol == null) throw new Exception("El rol del usuario es inválido");
+
+                Usuario u = new Usuario();
+                u.IdRol = rol;
+                u.Nombre = usuario.Nombre;
+                u.ApellidoPaterno = usuario.ApellidoPaterno;
+                u.ApellidoMaterno = usuario.ApellidoMaterno;
+                u.Correo = usuario.Correo;
+                u.Password = usuario.Password;
+                await u.Guardar();
+                return Ok(u);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToLista());
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> Obtener(int id)
+        {
+            try
+            {
+                if (id <= 0) throw new Exception("Usuario no válido");
+                Usuario usuario = await Usuario.ObtenerPorId(id);
+                if (usuario == null) throw new Exception("Usuario inexistente");
+                return Ok(usuario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToLista());
+            }
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Obtener()
+        {
+            try
+            {
+                List<Usuario> usuarios = await Usuario.ObtenerTodos();
+                return Ok(usuarios);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToLista());
+            }
+        }
+
+        [HttpPut]
+        [Route("")]
+        public async Task<IActionResult> Editar(UsuarioRequest usuario)
+        {
+            try
+            {
+                return Ok(usuario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToLista());
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Desactivar(int id)
+        {
+            try
+            {
+                return Ok("desactivar usuario: " + id);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToLista());
             }
         }
     }
