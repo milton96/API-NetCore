8f5c01b099a657238ca697cdc3a7a9b15c9787f2
Milton Bravo <miltonbravo96@gmail.com>
2021-06-18 14:42:19 -0500

    obtener rol por código

--- API JWT NETCORE/Models/Rol.cs
+++ API JWT NETCORE/Models/Rol.cs
@@ -1,16 +1,65 @@
-﻿using System;
+﻿using API_JWT_NETCORE.Helpers;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API_JWT_NETCORE.Models
 {
-    public class Rol
+    public class Rol : Conexion
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
         public int Codigo { get; set; }
         public bool Activo { get; set; }
+
+        public static async Task<Rol> ObtenerPorCodigo(int codigo)
+        {
+            Rol r = null;
+            try
+            {
+                string query = @"SELECT * FROM [dbo].[Rol] R WHERE R.Codigo = @Codigo";
+                using (SqlConnection con = Conectar())
+                {
+                    using (SqlCommand command = new SqlCommand(query, con)
+                    {
+                        CommandType = CommandType.Text,
+                        CommandTimeout = 60
+                    })
+                    {
+                        command.Parameters.AddWithValue("@Codigo", codigo);
+                        con.Open();
+                        using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                        {
+                            var i = new
+                            {
+                                Id = reader.GetOrdinal("Id"),
+                                Nombre = reader.GetOrdinal("Nombre"),
+                                Codigo = reader.GetOrdinal("Codigo"),
+                                Activo = reader.GetOrdinal("Activo")
+                            };
+
+                            if (reader.Read())
+                            {
+                                r = new Rol();
+                                r.Id = reader.GetValor<int>(i.Id);
+                                r.Nombre = reader.GetValor<string>(i.Nombre);
+                                r.Codigo = reader.GetValor<int>(i.Codigo);
+                                r.Activo = reader.GetValor<bool>(i.Activo);
+                            }
+                        }
+                        con.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return r;
+        }
     }
 
     public enum Roles
